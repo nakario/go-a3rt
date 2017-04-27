da19e0c3081651289923cd52f4885800f76194c5
Unknown <nakario.jp@gmail.com>
2017-04-27 13:11:15 +0900

    Add func NewClient

--- a3rt.go
+++ a3rt.go
@@ -1,1 +1,11 @@
-package go_a3rt
+package a3rt
+
+const apiBase string = "https://api.a3rt.recruit-tech.co.jp/"
+
+type Client struct{
+	key	string
+}
+
+func NewClient(key sting) Client {
+	return Client{key }
+}
