745066ac78539f104a0571cd1c818b7908d95a02
Unknown <nakario.jp@gmail.com>
2017-04-27 15:46:22 +0900

    Add predict

--- text_suggest.go
+++ text_suggest.go
@@ -1,1 +1,52 @@
 package a3rt
+
+import (
+	"fmt"
+	"net/url"
+)
+
+const maxPreviousDescriptionSize = 500
+
+type Style int
+const (
+	Gendaibun Style = iota
+	Waka
+	Go
+)
+
+type Separation int
+const (
+	Word Separation = iota
+	Phrase
+	Sentence
+)
+
+type PredictResponse struct{
+	Status		int		`json:"status"`
+	Message		string		`json:"message"`
+	Suggestion	[]string	`json:"suggestion"`
+}
+
+func (cli Client) Predict(previousDescription string, style Style, separation Separation) ([]string, error) {
+	if len(previousDescription) > maxPreviousDescriptionSize {
+		return nil, fmt.Errorf("request entity too long: previous description must not be more than %d characters.", maxPreviousDescriptionSize)
+	}
+
+	values := url.Values{}
+	values.Set("apikey", cli.key)
+	values.Add("previous_description", previousDescription)
+	values.Set("style", string(style))
+	values.Set("separation", string(separation))
+	var predict PredictResponse
+	err := cli.get("https://api.a3rt.recruit-tech.co.jp/text_suggest/v1/predict", values, &predict)
+	if err != nil {
+		return nil, err
+	}
+
+	switch predict.Status {
+	case 0:
+		return predict.Suggestion, nil
+	default:
+		return nil, fmt.Errorf(predict.Message)
+	}
+}
