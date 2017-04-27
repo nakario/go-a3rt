e3c0f850065f0ce7a720b0b117a238a73df8b75b
Unknown <nakario.jp@gmail.com>
2017-04-27 15:44:28 +0900

    Add utility functions

--- a3rt.go
+++ a3rt.go
@@ -1,5 +1,12 @@
 package a3rt
 
+import (
+	"net/url"
+	"net/http"
+	"io/ioutil"
+	"encoding/json"
+)
+
 const apiBase string = "https://api.a3rt.recruit-tech.co.jp/"
 
 type Client struct{
@@ -8,4 +15,44 @@ type Client struct{
 
 func NewClient(key string) Client {
 	return Client{key }
+}
+
+func (cli Client) get(endpoint string, values url.Values, resp interface{}) error {
+	getResp, err := http.Get(endpoint + "?" + values.Encode())
+	if err != nil {
+		return err
+	}
+	defer getResp.Body.Close()
+
+	bytes, err := ioutil.ReadAll(getResp.Body)
+	if err != nil {
+		return err
+	}
+
+	err = json.Unmarshal(bytes, resp)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (cli Client) post(endpoint string, values url.Values, resp interface{}) error {
+	postResp, err := http.PostForm(endpoint, values)
+	if err != nil {
+		return err
+	}
+	defer postResp.Body.Close()
+
+	bytes, err := ioutil.ReadAll(postResp.Body)
+	if err != nil {
+		return err
+	}
+
+	err = json.Unmarshal(bytes, resp)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
